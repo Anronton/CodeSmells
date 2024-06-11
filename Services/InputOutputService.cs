69bf27f5603fb7409c193ff8712a24136d2f92c6
Anton Eneroth <anton.eneroth@gmail.com>
2024-06-11 13:54:19 +0200

    Created IOService and will now apply it inside the GameController

---
+++ Services/InputOutputService.cs
@@ -0,0 +1,20 @@
+﻿using CodeSmells.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSmells.Services;
+
+public class InputOutputService : IInputOutput
+{
+    public string ReadLine()
+    {
+        return Console.ReadLine().Trim();
+    }
+    public void WriteLine(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
